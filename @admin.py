900db2b7bb3ab5e14934478d54d9c3fccdb1e38a
GarismoHussien <Hand2land@gmail.com>
2024-02-02 07:40:35 +0200

    T

---
+++ @admin.py
@@ -0,0 +1,14 @@
+@admin.register()
+class Admin(admin.ModelAdmin):
+    '''Admin View for '''
+
+    list_display = ('',)
+    list_filter = ('',)
+    inlines = [
+        Inline,
+    ]
+    raw_id_fields = ('',)
+    readonly_fields = ('',)
+    search_fields = ('',)
+    date_hierarchy = ''
+    ordering = ('',)
